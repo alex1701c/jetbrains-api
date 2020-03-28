865a7fad6f526ada3ea85b88316ae5e554c55b63
alex1701c <alex1701c.dev@gmx.net>
2020-03-28 13:47:44 +0100

    Build static libray for api, move export to cpp file

---
+++ export.cpp
@@ -0,0 +1,40 @@
+#include "export.h"
+
+namespace JetbrainsAPI {
+QList<JetbrainsApplication *> fetchApplications(const KConfigGroup &config, bool filterEmpty, bool fileWatchers) {
+    QList<JetbrainsApplication *> appList;
+    QList<JetbrainsApplication *> automaticAppList;
+    const auto mappingMap = config.group(Config::customMappingGroup).entryMap();
+    const auto desktopPaths = JetbrainsApplication::getInstalledApplicationPaths(
+        config.group(Config::customMappingGroup));
+
+    // Split manually configured and automatically found apps
+    for (const auto &p:desktopPaths.toStdMap()) {
+        // Desktop file is manually specified
+        if (mappingMap.contains(p.second)) {
+            auto customMappedApp = new JetbrainsApplication(p.second, fileWatchers);
+            QFile xmlConfigFile(mappingMap.value(p.second));
+            if (xmlConfigFile.open(QFile::ReadOnly)) {
+                customMappedApp->parseXMLFile(xmlConfigFile.readAll());
+                // Add path for filewatcher
+                customMappedApp->addPath(mappingMap.value(p.second));
+                if (!filterEmpty || !customMappedApp->recentlyUsed.isEmpty()) {
+                    appList.append(customMappedApp);
+                }
+            }
+            xmlConfigFile.close();
+        } else {
+            automaticAppList.append(new JetbrainsApplication(p.second, fileWatchers));
+        }
+    }
+
+    // Find automatically config directory, read config file and filter apps
+    SettingsDirectory::findCorrespondingDirectories(SettingsDirectory::getSettingsDirectories(), automaticAppList);
+    JetbrainsApplication::parseXMLFiles(automaticAppList);
+    if (filterEmpty){
+        automaticAppList = JetbrainsApplication::filterApps(automaticAppList);
+    }
+    appList.append(automaticAppList);
+    return appList;
+}
+}
