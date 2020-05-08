8ce2416f43e2325784e6b9000253afd632825af4
alex1701c <alex1701c.dev@gmx.net>
2020-05-08 03:59:30 +0200

    Fix missing git add

---
+++ Project.h
@@ -0,0 +1,14 @@
+#ifndef JETBRAINSRUNNER_PROJECT_H
+#define JETBRAINSRUNNER_PROJECT_H
+
+#include <QString>
+
+class Project {
+public:
+    QString name;
+    QString path;
+};
+
+
+
+#endif //JETBRAINSRUNNER_PROJECT_H
