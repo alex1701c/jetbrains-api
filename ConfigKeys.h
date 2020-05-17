b04c734b9078580bf56c16f2e93b7b3ff6e6691d
alex1701c <alex1701c.dev@gmx.net>
2020-05-17 12:50:14 +0200

    Add enum for search result filtering

--- ConfigKeys.h
+++ ConfigKeys.h
@@ -10,12 +10,23 @@ struct Config {
     constexpr static const auto displayInCategories = "DisplayInCategories";
     constexpr static const auto customMappingGroup = "CustomMapping";
     constexpr static const auto checkedUpdateDate = "checkedUpdateDate";
-    constexpr static const auto filterSearchResults = "filterSearchResults ";
+    constexpr static const auto filterSearchResults = "filterSearchResults";
 };
+
+// Text display inside KRunner
 struct FormatString {
     constexpr static const auto PROJECT = "%PROJECT";
     constexpr static const auto APPNAME = "%APPNAME";
     constexpr static const auto APP = "%APP";
     constexpr static const auto DIR = "%DIR";
 };
+
+// Used for filtering the project in KRunner
+enum SearchResultChoice {
+   ProjectNameContains,
+   PathContains,
+   ProjectNameStartsWith
+};
+Q_ENUMS(SearchResultChoice)
+
 #endif //JETBRAINSRUNNER_CONFIGKEYS_H
